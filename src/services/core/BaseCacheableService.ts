58a385fb4763152490ed9657ca3c99f3081993bb
Angel S. Moreno <angelxmoreno@gmail.com>
2025-11-09 22:36:30 -0500

    feat: created core classes for easier instantiation - migration of other classes has been documented

---
+++ src/services/core/BaseCacheableService.ts
@@ -0,0 +1,23 @@
+import { BaseLoggableService } from '@app/services/core/BaseLoggableService';
+import type Keyv from '@keyvhq/core';
+import type { Logger } from 'pino';
+
+export abstract class BaseCacheableService extends BaseLoggableService {
+    protected cache: Keyv;
+
+    protected constructor(logger: Logger, cache: Keyv) {
+        super(logger);
+        this.cache = cache;
+    }
+
+    protected async rememberCache<T = unknown>(cacheKey: string, func: () => Promise<T>, ttlMs = 1000): Promise<T> {
+        const cached = await this.cache.get(cacheKey);
+        if (cached !== undefined) {
+            return cached as T;
+        }
+
+        const result = await func();
+        await this.cache.set(cacheKey, result, ttlMs);
+        return result;
+    }
+}
